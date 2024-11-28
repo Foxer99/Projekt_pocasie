10a244c011fae440b128d9fd100f27f49e18e072
Filip Roman Michel <michelfiliproman@gmail.com>
2024-11-28 22:43:11 +0100

    Pdmienka pre mesiac

--- Projekt-počasie/Program.cs
+++ Projekt-počasie/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Authentication;
 using System.Text;
@@ -14,68 +15,106 @@ namespace Projekt_počasie
 
             Random random_nubmer = new Random();
 
+            List<string> dvadsatdevat = new List<string>();
+            dvadsatdevat.Add("Február");
+            dvadsatdevat.Add("Februar");
+            dvadsatdevat.Add("február");
+            dvadsatdevat.Add("februar");
+
             List<string> tridstat = new List<string>();
-            tridstat.Add("január");
-            tridstat.Add("januar");
-            tridstat.Add("Január");
-            tridstat.Add("Januar");
-            tridstat.Add("Marec");
-            tridstat.Add("marec");
-            tridstat.Add("Máj");
-            tridstat.Add("Maj");
-            tridstat.Add("máj");
-            tridstat.Add("maj");
-            tridstat.Add("Júl");
-            tridstat.Add("Jul");
-            tridstat.Add("júl");
-            tridstat.Add("jul");
-            tridstat.Add("August");
-            tridstat.Add("august");
-            tridstat.Add("Október");
-            tridstat.Add("Oktober");
-            tridstat.Add("október");
-            tridstat.Add("oktober");
-            tridstat.Add("December");
-            tridstat.Add("december");
+            tridstat.Add("Apríl");
+            tridstat.Add("April");
+            tridstat.Add("apríl");
+            tridstat.Add("april");
+            tridstat.Add("Jún");
+            tridstat.Add("Jun");
+            tridstat.Add("jún");
+            tridstat.Add("jun");
+            tridstat.Add("September");
+            tridstat.Add("september");
+            tridstat.Add("November");
+            tridstat.Add("november");
 
             List<string> tridstat_jedna = new List<string>();
-            tridstat_jedna.Add("Apríl");
-            tridstat_jedna.Add("April");
-            tridstat_jedna.Add("apríl");
-            tridstat_jedna.Add("april");
-            tridstat_jedna.Add("Jún");
-            tridstat_jedna.Add("Jun");
-            tridstat_jedna.Add("jún");
-            tridstat_jedna.Add("jun");
-            tridstat_jedna.Add("September");
-            tridstat_jedna.Add("september");
-            tridstat_jedna.Add("November");
-            tridstat_jedna.Add("november");
+            tridstat_jedna.Add("január");
+            tridstat_jedna.Add("januar");
+            tridstat_jedna.Add("Január");
+            tridstat_jedna.Add("Januar");
+            tridstat_jedna.Add("Marec");
+            tridstat_jedna.Add("marec");
+            tridstat_jedna.Add("Máj");
+            tridstat_jedna.Add("Maj");
+            tridstat_jedna.Add("máj");
+            tridstat_jedna.Add("maj");
+            tridstat_jedna.Add("Júl");
+            tridstat_jedna.Add("Jul");
+            tridstat_jedna.Add("júl");
+            tridstat_jedna.Add("jul");
+            tridstat_jedna.Add("August");
+            tridstat_jedna.Add("august");
+            tridstat_jedna.Add("Október");
+            tridstat_jedna.Add("Oktober");
+            tridstat_jedna.Add("október");
+            tridstat_jedna.Add("oktober");
+            tridstat_jedna.Add("December");
+            tridstat_jedna.Add("december");
 
-            string februar = "29";
+            string mesiac = null;
+            int dvadsatdevat_s = 0;
+            int tridstat_s = 0;
+            int tridstat_jedna_s = 0;
+            
+            string den_s = null;
+            int den_i = 0;
+            
+            string teplota_s = null;
+            double teplota_d = 0;
 
-            Console.WriteLine("Zadaj deň v mesiaci: ");
-            string den_s = Console.ReadLine();
-            int den_i = int.Parse(den_s);
-
-            Console.WriteLine("Zadaj mesiac");
-            string mesiac = Console.ReadLine();
-
-            Console.WriteLine("Zadaj teplotu: ");
-            string teplota_s = Console.ReadLine();
-            double teplota_d = double.Parse(teplota_s);
-
-            Console.WriteLine(den_i + ". " + mesiac + " a teplota je " + teplota_d + "°C");
-
-            for (int i = 0; i < 100; i++)
+            do
             {
-                double teplota = int.Parse(teplota_s);
-                double r_n = random_nubmer.NextDouble();
-                teplota = teplota + r_n;
-                teplota = (double)Math.Round(teplota,1);
-                Console.WriteLine(teplota);
-
+                Console.WriteLine("Zadaj mesiac");
+                mesiac = Console.ReadLine();
+                if (dvadsatdevat.Contains(mesiac))
+                {
+                    dvadsatdevat_s = 1;
+                    break;
+                }
+                else if (tridstat.Contains(mesiac))
+                {
+                    tridstat_s = 1;
+                    break;
+                }
+                else if (tridstat_jedna.Contains(mesiac))
+                {
+                    tridstat_jedna_s = 1;
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("\n!!!Error!!!\n> Zadal si zlý mesiac <\n");
+                }
             }
+            while (true);
+
+                Console.WriteLine("Zadaj deň v mesiaci: ");
+                den_s = Console.ReadLine();
+                den_i = int.Parse(den_s);
+
+                Console.WriteLine("Zadaj teplotu: ");
+                teplota_s = Console.ReadLine();
+                teplota_d = double.Parse(teplota_s);
+
+                Console.WriteLine(den_i + ". " + mesiac + " a teplota je " + teplota_d + "°C");
+
+                for (int i = 0; i < 100; i++)
+                {
+                    double teplota = int.Parse(teplota_s);
+                    double r_n = random_nubmer.NextDouble();
+                    teplota = teplota + r_n;
+                    teplota = (double)Math.Round(teplota, 1);
+                    Console.WriteLine(teplota);
+                }
+           
         }
     }
 }
